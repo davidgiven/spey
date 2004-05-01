1d6cd43cd6aac0077d8615d6babba8764c2232ab
David Given <dg@cowlark.com>
2004-05-01 12:19:49 +0000

    Adjusted boilerplate.

--- src/Exception.cc
+++ src/Exception.cc
@@ -1,5 +1,11 @@
 /* Exception.cc
  * Exception handler base class
+ *
+ * Copyright (C) 2004 David Given
+ * You may distribute under the terms of the GNU General Public
+ * License version 2 as specified in the file COPYING that comes with the
+ * Spey distribution.
+ *
  * $Source$
  * $State$
  */
@@ -30,4 +36,6 @@ Exception::Exception(string reason, int errno)
 
 /* Revision history
  * $Log$
+ * Revision 1.1  2004/05/01 11:52:00  dtrg
+ * Initial version.
  */
