183110db64b96d3d084cc79484ea9b16f7cbcc6b
David Given <dg@cowlark.com>
2004-06-09 18:40:36 +0000

    Fixed some tracing where the address of incoming connections was being reported incorrectly in the logs (but correctly in the Received lines of incoming messages).

--- src/Socket.h
+++ src/Socket.h
@@ -26,7 +26,7 @@ struct Socket {
 	string readline();
 	void writeline(string l);
 
-	void setaddress(const SocketAddress& address) { _address = address; }
+	void setaddress(const SocketAddress& address);
 	const SocketAddress& getaddress() { return _address; }
 	int timeout() { return _timeout; }
 	void timeout(int t) { _timeout = t; }
@@ -43,6 +43,11 @@ protected:
 
 /* Revision history
  * $Log$
+ * Revision 1.5  2004/06/08 19:58:04  dtrg
+ * Fixed a bug where the address of incoming connections was thought to be the
+ * address of *this* end of the connection, not the other end. In the process,
+ * changed some this->blah instance variables to _blah.
+ *
  * Revision 1.4  2004/05/30 01:55:13  dtrg
  * Numerous and major alterations to implement a system for processing more than
  * one message at a time, based around coroutines. Fairly hefty rearrangement of
